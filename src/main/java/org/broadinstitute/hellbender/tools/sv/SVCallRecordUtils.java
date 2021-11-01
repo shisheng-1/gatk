275372a73b6f2dec03b00ad2f371b3f055f2c357
Mark Walker <markw@broadinstitute.org>
2021-11-01 13:04:51 -0400

    Copy over util functions; restore vcf constants

--- src/main/java/org/broadinstitute/hellbender/tools/sv/SVCallRecordUtils.java
+++ src/main/java/org/broadinstitute/hellbender/tools/sv/SVCallRecordUtils.java
@@ -2,6 +2,7 @@ package org.broadinstitute.hellbender.tools.sv;
 
 import com.google.common.collect.Sets;
 import htsjdk.samtools.SAMSequenceDictionary;
+import htsjdk.samtools.SAMSequenceRecord;
 import htsjdk.samtools.util.Locatable;
 import htsjdk.variant.variantcontext.*;
 import htsjdk.variant.vcf.VCFConstants;
@@ -406,5 +407,94 @@ public final class SVCallRecordUtils {
     // TODO this is sort of hacky but the Allele compareTo() method doesn't give stable ordering
     public static List<Allele> sortAlleles(final Collection<Allele> alleles) {
         return alleles.stream().sorted(Comparator.nullsFirst(Comparator.comparing(Allele::getDisplayString))).collect(Collectors.toList());
+    }
+
+
+    // TODO for a later pr
+    public static <T extends SVLocatable> Comparator<T> getSVLocatableComparatorByEnds(final SAMSequenceDictionary dictionary) {
+        return (o1, o2) -> compareSVLocatablesByEnds(o1, o2, dictionary);
+    }
+
+    // TODO for a later pr
+    public static int compareSVLocatablesByEnds(final SVLocatable first, final SVLocatable second, final SAMSequenceDictionary dictionary) {
+        Utils.nonNull(first);
+        Utils.nonNull(second);
+        // First locus
+        final Comparator<Locatable> locatableComparator = IntervalUtils.getDictionaryOrderComparator(dictionary);
+        // Second locus
+        final int compareB = locatableComparator.compare(new SimpleInterval(first.getContigB(), first.getPositionB(), first.getPositionB()),
+                new SimpleInterval(second.getContigB(), second.getPositionB(), second.getPositionB()));
+        return compareB;
+    }
+
+    // TODO : for a later PR
+    public static VariantContextBuilder createBuilderWithEvidence(final SVCallRecordWithEvidence call, boolean updateSplitRead, boolean updateDiscordantPair) {
+        final VariantContextBuilder builder = getVariantBuilder(call);
+        final boolean includeEvidence = !call.isDepthOnly();
+        final SplitReadSite startSplitReadCounts = includeEvidence ? getSplitReadCountsAtPosition(call.getStartSplitReadSites(), call.getPositionA()) : null;
+        final SplitReadSite endSplitReadCounts = includeEvidence ? getSplitReadCountsAtPosition(call.getEndSplitReadSites(), call.getPositionB()) : null;
+        final Map<String,Integer> discordantPairCounts = includeEvidence ? getDiscordantPairCountsMap(call.getDiscordantPairs()) : null;
+        final List<Genotype> genotypes = builder.getGenotypes();
+        final List<Genotype> newGenotypes = new ArrayList<>(genotypes.size());
+        for (final Genotype genotype : genotypes) {
+            final String sample = genotype.getSampleName();
+            final GenotypeBuilder genotypeBuilder = new GenotypeBuilder(genotype);
+            if (includeEvidence) {
+                if (updateSplitRead) {
+                    genotypeBuilder.attribute(GATKSVVCFConstants.START_SPLIT_READ_COUNT_ATTRIBUTE, startSplitReadCounts.getCount(sample));
+                    genotypeBuilder.attribute(GATKSVVCFConstants.END_SPLIT_READ_COUNT_ATTRIBUTE, endSplitReadCounts.getCount(sample));
+                }
+                if (updateDiscordantPair) {
+                    genotypeBuilder.attribute(GATKSVVCFConstants.DISCORDANT_PAIR_COUNT_ATTRIBUTE, discordantPairCounts.getOrDefault(sample, 0));
+                }
+            }
+            newGenotypes.add(genotypeBuilder.make());
+        }
+        builder.genotypes(newGenotypes);
+        return builder;
+    }
+
+    // TODO for a later pr
+    private static SplitReadSite getSplitReadCountsAtPosition(final List<SplitReadSite> sites, final int pos) {
+        Utils.nonNull(sites);
+        Utils.validateArg(pos > 0, "Non-positive position");
+        if (sites.stream().map(SplitReadSite::getPosition).distinct().count() != sites.size()) {
+            throw new IllegalArgumentException("Sites did not have unique positions");
+        }
+        return sites.stream()
+                .filter(s -> s.getPosition() == pos)
+                .findAny()
+                .orElse(new SplitReadSite(pos, Collections.emptyMap()));
+    }
+
+    // TODO for a later pr
+    private static Map<String,Integer> getDiscordantPairCountsMap(final Collection<DiscordantPairEvidence> discordantPairs) {
+        Utils.nonNull(discordantPairs);
+        return discordantPairs.stream()
+                .collect(Collectors.groupingBy(DiscordantPairEvidence::getSample,
+                        Collectors.reducing(0, e -> 1, Integer::sum)));
+    }
+
+    // TODO for a future pr
+    public static void validateCoordinates(final SVCallRecord call) {
+        Utils.nonNull(call);
+        Utils.validateArg(call.getPositionA() >= 1, "Call start non-positive");
+        if (call.isIntrachromosomal()) {
+            Utils.validateArg(call.getPositionA() <= call.getPositionB(), "Second position before end on same contig");
+        } else {
+            Utils.validateArg(call.getPositionB() >= 1, "Call second position non-positive");
+        }
+    }
+
+    // TODO for a future pr
+    public static void validateCoordinatesWithDictionary(final SVCallRecord call, final SAMSequenceDictionary dictionary) {
+        Utils.nonNull(dictionary);
+        validateCoordinates(call);
+        final SAMSequenceRecord contigARecord = dictionary.getSequence(call.getContigA());
+        Utils.validateArg(contigARecord != null, "Call first contig " + call.getContigA() + " not in dictionary");
+        final SAMSequenceRecord contigBRecord = dictionary.getSequence(call.getContigB());
+        Utils.validateArg(contigBRecord != null, "Call second contig " + call.getContigB() + " not in dictionary");
+        Utils.validateArg(call.getPositionA() <= contigARecord.getSequenceLength(), "Call first position greater than contig length");
+        Utils.validateArg(call.getPositionB() <= contigBRecord.getSequenceLength(), "Call second position greater than contig length");
     }
 }
